afbbefe3d17e274ed7e1b4a84a439125f99827d8
karirin <karirin419@gmail.com>
2022-12-19 19:05:52 +0900

    ''

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -57,7 +57,7 @@ class UserController extends Controller
     {
         $form = [
             'name' => $request->name,
-            'password' => $request->hash_password,
+            'password' => $request->password,
             'image' => $request->image,
             'age' => $request->age,
             'address' => $request->address,
@@ -88,7 +88,6 @@ class UserController extends Controller
             $param = ['current_user' => $current_user, 'users' => $users, 'skills' => $skills, 'licences' => $licences, 'message_count' => $message_count, 'message' => $message, 'top_message' => $top_message, 'match_flg' => $match_flg];
             return view('user.add_match', $param);
         } else {
-            $current_usersasassa;
             $param = ['current_user' => $current_user, 'users' => $users, 'skills' => $skills, 'licences' => $licences, 'message_count' => $message_count, 'message' => $message, 'top_message' => $top_message, 'match_flg' => $match_flg];
             return view('user.add_match', $param);
         }
