ad2f8c6ac07c15e80fed57acd7adc6d603193261
karirin <karirin419@gmail.com>
2022-12-25 15:16:22 +0900

    ''

--- database/migrations/2022_12_21_103214_message_relations.php
+++ database/migrations/2022_12_21_103214_message_relations.php
@@ -14,7 +14,8 @@ class MessageRelations extends Migration
     public function up()
     {
         Schema::create('message_relations', function (Blueprint $table) {
-            $table->integer('user_id')->primary();
+            $table->bigIncrements('id');
+            $table->integer('user_id');
             $table->integer('destination_user_id');
             $table->text('message_count')->nullable(true);
         });
