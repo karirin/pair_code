3c7208238f4c0effc2784063efb18a2daa965554
karirin <karirin419@gmail.com>
2023-01-03 10:03:10 +0900

    ''

--- public/js/common.js
+++ public/js/common.js
@@ -1767,6 +1767,31 @@ $(document).on('click', '#before', function() {
     $('.woman').fadeIn();
 });
 
+/**
+* タッチ操作での拡大縮小禁止
+*/
+function no_scaling() {
+    document.addEventListener("touchmove", mobile_no_scroll, { passive: false });
+}
+
+/**
+* タッチ操作での拡大縮小禁止解除
+*/
+function return_scaling() {
+    document.removeEventListener('touchmove', mobile_no_scroll, { passive: false });
+}
+
+/**
+* 拡大縮小禁止
+*/
+function mobile_no_scroll(event) {
+    // ２本指での操作の場合
+    if (event.touches.length >= 2) {
+        // デフォルトの動作をさせない
+        event.preventDefault();
+    }
+}
+
 // 各種ツールチップ処理
 $('[data-toggle="favorite"]').tooltip();
 $('[data-toggle="post"]').tooltip();
