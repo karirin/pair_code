e94a9486c68786ff8621b6781c34a6a775ddd4b9
karirin <karirin419@gmail.com>
2023-01-09 22:27:20 +0900

    ''

--- resources/views/user/add_match.blade.php
+++ resources/views/user/add_match.blade.php
@@ -159,12 +159,12 @@
                     setInterval(function() {
                         $('.pointer2').animate({
                             'left': '72%',
-                            'top': '64%'
+                            'top': '68%'
                         });
                         $('.pointer2').fadeOut();
                         $('.pointer2').animate({
                             'left': '60%',
-                            'top': '68%'
+                            'top': '72%'
                         });
                         $('.pointer2').fadeIn();
                     }, 1000);
@@ -309,12 +309,12 @@
                 setInterval(function() {
                     $('.pointer2').animate({
                         'left': '72%',
-                        'top': '64%'
+                        'top': '68%'
                     });
                     $('.pointer2').fadeOut();
                     $('.pointer2').animate({
                         'left': '60%',
-                        'top': '68%'
+                        'top': '72%'
                     });
                     $('.pointer2').fadeIn();
                 }, 1000);
