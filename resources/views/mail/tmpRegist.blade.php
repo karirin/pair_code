ab071cd1d8c0f21353e35c546881c53f61e2b60b
karirin <karirin419@gmail.com>
2023-02-04 12:26:43 +0900

    'メール本文の表示をスマホ対応'

--- resources/views/mail/tmpRegist.blade.php
+++ resources/views/mail/tmpRegist.blade.php
@@ -1,5 +1,5 @@
 <head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     @if(app('env') == 'production')
     <link href="{{ secure_asset('css/style.css') }}" rel="stylesheet">
     <link href="{{ secure_asset('css/match.css') }}" rel="stylesheet">
@@ -8,10 +8,11 @@
     <link href="{{ asset('css/match.css') }}" rel="stylesheet">
     @endif
 </head>
+
 <body>
-        <div style="margin: 0 24%;">
-<h2>Pair Codeへのご登録ありがとうございます！</h2>
-<span>下のURLをクリックして、メールアドレスの認証を完了してください。</span>
-<div style="text-align: center;"><a href="{{$url}}" class="btn-square">認証を完了する</a></div>
-</div>
+    <div class="saling_mail" style="margin: 0 24%;">
+        <h2>Pair Codeへのご登録ありがとうございます！</h2>
+        <span>下のURLをクリックして、メールアドレスの認証を完了してください。</span>
+        <div style="text-align: center;"><a href="{{$url}}" class="btn-square">認証を完了する</a></div>
+    </div>
 </body>
